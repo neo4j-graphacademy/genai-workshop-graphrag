3dc6cfbc98762a09397ae02941f02f188cb39304
Martin O'Hanlon <martin.ohanlon@neo4j.com>
2024-03-20 14:41:27 +0000

    updated check env

--- check_env.py
+++ check_env.py
@@ -2,8 +2,10 @@ import os
 from colorama import init, Fore, Style
 init()
 
+SETUP_URL = "https://graphacademy.neo4j.com/courses/genai-workshop/1-knowledge-graphs-vectors/1-getting-started/"
+
 env_file_exists = os.path.exists('.env')
 if not env_file_exists:
     print(Fore.RED + "Environment .env file not found.")
-    print(Fore.BLUE + "Goto https://graphacademy.neo4j.com/courses/genai-workshop/1-knowledge-graphs-vectors/1-getting-started/ for instructions on setting up your environment.")
+    print(Fore.BLUE + f"Goto {SETUP_URL} for instructions on setting up your environment.")
     print(Style.RESET_ALL)
